40a0f0a8a449d160e6ad875bcf1a195d1a1d64a8
eloahluara <eloahluaraa123@gmail.com>
2024-08-20 12:29:15 -0300

    atualizando

--- ExemploHttp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ExemploHttp.AssemblyInfo.cs
+++ ExemploHttp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ExemploHttp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploHttp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8c6c5e1f36ffb932d5d417fe2248885d515e8a49")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploHttp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploHttp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
